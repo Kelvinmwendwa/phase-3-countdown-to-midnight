7eeb39216e49102835319b73ae25b0a1924296dc
Kelvin <kmwendwa476@gmail.com>
2022-08-19 15:35:08 +0300

    commit tree 4adac5754b518d2f5af59cf05e677f1a4831ed37

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 6
+  end
+  "HAPPY NEW YEAR!"
+end
+ 
+puts countdown_with_sleep(4)
